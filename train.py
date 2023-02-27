83f4aa1b6355ca2d54d72bdbb3bba26072bc240f
Benjamin Anderson <andersonbcdefg@gmail.com>
2023-02-26 18:30:10 -0800

    was doing max batch size instead of micro_batch_size

--- train.py
+++ train.py
@@ -231,7 +231,7 @@ def train_bert(bert_config, train_config):
                 )
             train_loader = torch.utils.data.DataLoader(
                 train_dataset, 
-                batch_size=train_config.max_batch_size,
+                batch_size=train_config.micro_batch_size,
                 shuffle=True,
                 num_workers=train_config.train_workers,
                 pin_memory=True
